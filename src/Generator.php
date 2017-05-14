a776e78d852966f344ec65f367d768a8705b0f31
Tayfun <tayfun@Tayfun-MacBook-Pro-2.local>
2017-05-14 22:30:31 +0300

    Revize

--- src/Generator.php
+++ src/Generator.php
@@ -1,15 +1,51 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tayfun
- * Date: 14/05/2017
- * Time: 10:32
+ * File:    Generator.php
+ * User:    tayfun
+ * Date:    14/05/2017
+ * Time:    22:23
  */
 
 namespace Bayt\Random;
 
+use Symfony\Component\Finder\Finder;
+
 
 class Generator
 {
+    /**
+     * @var Generator
+     */
+    protected $generator;
+
+    /**
+     * @var array|null
+     */
+    private $languages = [];
+
+    /**
+     * @var Generator
+     */
+    protected $finder;
+
+    /**
+     * Generator constructor.
+     * @param $generator
+     * @param $languages
+     */
+    public function __construct(Generator $generator, $languages = null)
+    {
+        $this->generator = $generator;
+        $this->languages = $languages ?? $languages;
+        $this->finder = $this->readFormatterFiles();
+    }
+
+    /**
+     * @return $this
+     */
+    private function readFormatterFiles()
+    {
+        return (new Finder())->in(__DIR__ . "/Formatters");
+    }
 
 }
